32c05ce2ef7ec31ab05f36a6532d92063df2e67c
George Alonge <georgedelson@gmail.com>
2017-03-06 21:59:18 +0100

    add active link states

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,14 @@ module ApplicationHelper
 	end
 
 
+	# def active_link_to(name = nil, options = nil, html_options = nil, &block)
+	#   active_class = html_options[:active] || "active"
+	#   html_options.delete(:active)
+	#   html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(options)
+	#   link_to(name, options, html_options, &block)
+	# end
+
+
 	# def custom_bootstrap_flash
 	# 	  flash_messages = []
 	# 	  flash.each do |type, message|
